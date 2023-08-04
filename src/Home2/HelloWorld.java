11857bdca6023ba89c811ac8b39826f0727de879
asavko <savkoaleks@gmail.com>
2023-08-04 09:32:48 +0300

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	.idea/vcs.xml

---
+++ src/Home2/HelloWorld.java
@@ -0,0 +1,7 @@
+package Home2;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, world");
+    }
+}
--- src/Home2/Hello_world.java
+++
@@ -1,7 +0,0 @@
-package Home2;
-
-public class Hello_world {
-    public static void main(String[] args) {
-        System.out.println("Home2.Hello. world");
-    }
-}
